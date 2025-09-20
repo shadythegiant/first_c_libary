9e9a0ac7df08b982c9d925080dcc3bb4836e99f9
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-09-20 17:20:25 +0100

    init

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -11,18 +11,20 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-size_t ft_strlcat(char *dest, const char *src, size_t size) {
-	size_t src_counter;
-	size_t dest_len; 
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	src_counter;
+	size_t	dest_len;
 
-	dest_len = 0; 
-	while(dest[dest_len] && dest_len < size) 
-		dest_len++; 
-	src_counter = 0; 
-	while(src[src_counter] && (dest_len + src_counter + 1) < size) {
-		dest[dest_len + src_counter] = src[src_counter]; 
-		src_counter++; 
+	dest_len = 0;
+	while (dest[dest_len] && dest_len < size)
+		dest_len++;
+	src_counter = 0;
+	while (src[src_counter] && (dest_len + src_counter + 1) < size)
+	{
+		dest[dest_len + src_counter] = src[src_counter];
+		src_counter++;
 	}
-	dest[dest_len + src_counter] = '\0'; 
-	return(dest_len + ft_strlen(src)); 
+	dest[dest_len + src_counter] = '\0';
+	return (dest_len + ft_strlen(src));
 }
