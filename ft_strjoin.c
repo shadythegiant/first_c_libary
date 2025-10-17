231b382f9fcd09a6a69e7190c170253e9448dabb
0rph3us <64805107+shadythegiant@users.noreply.github.com>
2025-10-17 14:04:00 +0000

    testingt

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,10 +1,10 @@
 #include "libft.h"
 
-static char *ft_concat(size_t size, char **strings, char *result) { 
+static char *ft_concat(size_t size, char const  **strings, char *result) { 
      size_t count; 
      size_t result_index; 
      size_t inner_c; 
-     char *str;       
+     char const *str;       
      count = 0; 
      result_index = 0; 
      while(count < size) { 
@@ -21,20 +21,22 @@ static char *ft_concat(size_t size, char **strings, char *result) {
 
 char *ft_strjoin(char const *s1, char const *s2) {
     size_t size; 
+    size_t arr_size; 
     char *result_s; 
-    char *strings[2]; 
+    char const *strings[2]; 
 
     size = ft_strlen(s1) + ft_strlen(s2); 
     if(size == 0) { 
-        result_s = malloc(sizeof(char) * 1): 
+        result_s = malloc(sizeof(char) * 1);
         result_s[0] = '\0'; 
         return (result_s); 
     }
     strings[0] = s1; 
     strings[1] = s2; 
+    arr_size = sizeof(strings) / sizeof(strings[0]); 
     result_s = malloc(sizeof(char) * (size + 1)); 
     if(!result_s) 
         return NULL; 
-    result_s = ft_concat(size, strings, result_s); 
+    result_s = ft_concat(arr_size, strings, result_s); 
     return(result_s); 
 }
