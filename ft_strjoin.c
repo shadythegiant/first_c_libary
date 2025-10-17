ec18c17c8a7627e90ac3d8bd26c5e9a1e54b0a71
0rph3us <64805107+shadythegiant@users.noreply.github.com>
2025-10-17 13:20:09 +0000

    trying to see if ft_strjoin works

---
+++ ft_strjoin.c
@@ -0,0 +1,40 @@
+#include "libft.h"
+
+static char *ft_concat(size_t size, char **strings, char *result) { 
+     size_t count; 
+     size_t result_index; 
+     size_t inner_c; 
+     char *str;       
+     count = 0; 
+     result_index = 0; 
+     while(count < size) { 
+        inner_c = 0; 
+        str = strings[count]; 
+        while(str[inner_c]) {
+            result[result_index++] = str[inner_c++]; 
+        }
+        count++; 
+     }
+     result[result_index]= '\0'; 
+     return (result); 
+}
+
+char *ft_strjoin(char const *s1, char const *s2) {
+    size_t size; 
+    char *result_s; 
+    char *strings[2]; 
+
+    size = ft_strlen(s1) + ft_strlen(s2); 
+    if(size == 0) { 
+        result_s = malloc(sizeof(char) * 1): 
+        result_s[0] = '\0'; 
+        return (result_s); 
+    }
+    strings[0] = s1; 
+    strings[1] = s2; 
+    result_s = malloc(sizeof(char) * (size + 1)); 
+    if(!result_s) 
+        return NULL; 
+    result_s = ft_concat(size, strings, result_s); 
+    return(result_s); 
+}
