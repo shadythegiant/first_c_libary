3d3788dadd435792706b798e49bab573ce540357
0rph3us <64805107+shadythegiant@users.noreply.github.com>
2025-10-19 13:43:27 +0000

    working and still stuck af

---
+++ ft_strtrim.c
@@ -0,0 +1,54 @@
+#include "libft.h"
+#include <stdio.h>
+
+static size_t ft_ischarset(char c, char const *set) {
+    size_t i; 
+    i = 0; 
+    while(set[i]) {
+        if(c == set[i] ) {
+            return 1; 
+        }
+        i++; 
+    }
+    return 0; 
+}
+char *ft_strtrim(char const *s1, char const *set) {
+    size_t result_index; 
+    size_t size; 
+    char *result;
+    char const *start ; 
+    char const *end; 
+
+    start = s1; 
+    end = (start + (ft_strlen(s1) -1) );
+
+    if(start > end) { 
+        result = malloc(1); 
+        if(!result)
+            return NULL; 
+        result[0] = '\0'; 
+        return result;     
+    }
+   
+    while(*start && ft_ischarset(*start, set) )
+        start++; 
+    
+    while(end >= start && ft_ischarset(*end, set))
+        end--; 
+     size = (end - start ) + 1; 
+    result = malloc(sizeof(char) * (size + 1)); 
+    if(!result)
+        return NULL; 
+    while(start <= end) {
+        result[result_index++] = *start++; 
+    }    
+    result[result_index] = '\0'; 
+    return(result); 
+}
+
+int main() { 
+    char *s = " -hello";
+    char *set = " -"; 
+    char *result = ft_strtrim(s, set); 
+    printf("%s", result);   
+}
--- ft_substr.c
+++ ft_substr.c
@@ -39,7 +39,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub = malloc(sizeof(char) * (sub_len + 1));
 	if (!sub)
 		return (NULL);
-	ft_memset(sub, s + start, sub_len);
+	ft_memset(sub, (s + start), sub_len);
 	sub[sub_len] = '\0';
 	return (sub);
 }
