f9740e091f0d543bb4e61fc4d64558b747178625
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-11-03 18:00:36 +0100

    fixing bugs

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -15,13 +15,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*temp;
 
-	while (lst != NULL)
+	if (!lst || !del)
+		return ;
+	while (*lst)
 	{
-		while (*lst)
-		{
-			temp = (*lst)->next;
-			ft_lstdelone(*lst, del);
-			*lst = temp;
-		}
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
 	}
 }
